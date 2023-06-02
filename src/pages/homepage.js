3a0dc227fb0e77e47d8dc29878da5f17a91df8ed
UBC Student <vdesh@students.cs.ubc.ca>
2023-06-01 20:59:30 -0700

    update
    changes

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -8,6 +8,10 @@ function homepage() {
             {/* factor out as a component later, if necessary*/}
             <div className="left-pane">
                 <p>Join a session</p>
+                <div className="session">
+                    <p className="session-id">xxx-xxxx</p>
+                    <p className="capacity">1/4</p>
+                </div>
             </div>
         </div>
     );
