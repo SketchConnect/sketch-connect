a99167bc5b4324f2621ac6527455282d7489273f
sthu <shutinghu2000@gmail.com>
2023-06-06 14:17:09 -0700

    about page file, remove redundant box

--- src/components/header.js
+++ src/components/header.js
@@ -45,55 +45,6 @@ function Header() {
       <Container maxWidth="xl">
         <Toolbar disableGutters>
 
-          {/* this box seems redundant? */}
-          {/* <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon style={{ fill: 'black' }} />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-            <img
-              src="/assets/images/logo.png"
-              alt="sketch connect logo"
-              style={{
-                display: "block",
-                marginLeft: "auto",
-                marginRight: "auto",
-                marginBottom: "10px",
-                borderRadius: 12
-              }}
-            />
-          </Box> */}
-
           <Box sx={{ flexGrow: 1, fontFamily: 'Chelsea Market', display: { xs: 'none', md: 'flex' } }}>
             {pages.map((page) => (
               <Button
@@ -108,7 +59,6 @@ function Header() {
             ))}
             <img className="logo" src="/assets/images/logo.png" alt="sketch connect logo" />
           </Box>
-
 
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
---
+++ src/pages/AboutPage.js
@@ -0,0 +1,5 @@
+function AboutPage() {
+    return <h1>About Us</h1>;
+}
+
+export default AboutPage;
