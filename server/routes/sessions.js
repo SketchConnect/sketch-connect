5469ac3ba969bdd1dbf7cc5a8c0b4ba4475d4fd7
martin cai <martincai46@gmail.com>
2023-06-09 23:53:37 -0700

    Reformat comments

--- server/routes/sessions.js
+++ server/routes/sessions.js
@@ -4,35 +4,35 @@ import db from "../db/conn.js";
 const router = express.Router();
 
 /**
-    GET /sessions
-    gets a list of all the sessions
-
-    Request body parameters:
-        - none
-
-    Returns:
-        - an array containing all the session objects in the database
-*/
+ * GET /sessions
+ * Gets a list of all the sessions.
+ *
+ * Request body parameters:
+ * - none
+ *
+ * Returns:
+ * - An array containing all the session objects in the database.
+ */
 router.get("/", async (req, res) => {
     let collection = await db.collection("sessions");
-    let results = await collection.find({}).toArray();
-    res.send(results).status(200);
+    let sessions = await collection.find({}).toArray();
+    res.status(200).send(sessions);
 });
 
 /**
-    POST /sessions
-    creates a new game session with the provided data.
-
-    Request body parameters:
-        - isPublic (boolean): Determines if the session is public
-        - status (string): The current status of the session. One of "waiting", "ongoing", or "completed", or "cancelled"
-        - players (array): An array of player names. Will be initialized with the host's name
-        - quadrant (array): Used to store links to each of the images, initially empty
-        - finalImage (string): Used to store the final image's URL or data, initially empty
-
-    Returns:
-        - a JSON object with a single property, _id, which contains the ID of the newly created session.
-*/
+ * POST /sessions
+ * Creates a new game session.
+ *
+ * Request body parameters:
+ * - isPublic (boolean): Determines if the session is public.
+ * - status (string): The current status of the session. One of "waiting", "ongoing", "completed", or "cancelled".
+ * - players (array): An array of player ids. Will be initialized with the host's id.
+ * - quadrant (array): Used to store links to each of the images, initially empty.
+ * - finalImage (string): Used to store the final image's URL or data, initially empty.
+ *
+ * Returns:
+ * - A JSON object with a single property, id, which contains the ID of the newly created session.
+ */
 router.post("/", async (req, res) => {
     let newDocument = {
         isPublic: req.body.isPublic,
@@ -43,7 +43,7 @@ router.post("/", async (req, res) => {
     };
     let collection = await db.collection("sessions");
     let result = await collection.insertOne(newDocument);
-    res.status(200).send({ _id: result.insertedId });
+    res.status(200).send({ id: result.insertedId });
 });
 
 export default router;
