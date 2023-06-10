6a123df288a4ee879c4814304e897bf45cac61cb
michelleykim <yeojin011016@gmail.com>
2023-06-10 01:41:38 -0700

    point app.js to correct place

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,33 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import "./App.css";
+import Header from "./components/header";
+import Homepage from "./pages/homepage";
+import AboutPage from "./pages/AboutPage";
+import WaitingPage from "./pages/WaitingPage";
+import GamePage from "./pages/GamePage";
+
+import { createTheme, ThemeProvider } from "@mui/material";
+
+const theme = createTheme({
+    typography: {
+        fontFamily: ["Chilanka", "cursive"].join(","),
+    },
+});
 
 function App() {
     return (
-        <div className="App">
-            <h1>Hello World</h1>
-        </div>
+        <>
+            <BrowserRouter>
+                <Header></Header>
+                <Switch>
+                    <Route exact path="/" component={Homepage} />
+                    <Route path="/about" component={AboutPage} />
+                    <Route path="/waiting" component={WaitingPage} />
+                    <Route path="/game" component={GamePage} />
+                </Switch>
+            </BrowserRouter>
+        </>
     );
 }
 
