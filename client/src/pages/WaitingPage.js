d6eb9446a002b09792463e0c0032584c07e538e7
sthu <shutinghu2000@gmail.com>
2023-06-21 18:33:17 -0700

    lobby page setup

--- client/src/pages/WaitingPage.js
+++ client/src/pages/WaitingPage.js
@@ -2,11 +2,30 @@ import "./WaitingPage.css";
 
 
 function WaitingPage() {
-    return (
-      <div className="App">
-        
-      </div>
-    );
+  const playerCount = 3; // temp hard coded, will retrieve from server based on players joined
+
+  let imageSource;
+  if (playerCount === 1) {
+    imageSource = 'player1.png';
+  } else if (playerCount === 2) {
+    imageSource = 'player2.png';
+  } else if (playerCount === 3) {
+    imageSource = 'player3.png';
+  } else {
+    imageSource = 'player4.png';
   }
 
-  export default WaitingPage;
+  return (
+    <div className="lobby-container">
+      <h2>Session xxxx-xxxx is waiting for players to join...</h2>
+      <div>
+        <img src={"/assets/images/players/" + imageSource} alt="lobby" />
+      </div>
+      <div>
+        
+      </div>
+    </div>
+  );
+}
+
+export default WaitingPage;
