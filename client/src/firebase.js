4b34c1fdfb0269fad2881ec12c41783460dbaa9b
Michelle W <michelle@Michelles-MacBook-Pro.local>
2023-06-23 23:46:09 -0700

    move firebaseConfig

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,23 +1,17 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBPorpPHRRtbLtSqYvV8rdnn82WIUG4W94",
-  authDomain: "sketch-connect.firebaseapp.com",
-  projectId: "sketch-connect",
-  storageBucket: "sketch-connect.appspot.com",
-  messagingSenderId: "925929268919",
-  appId: "1:925929268919:web:f59cc3c96d54b349688f32",
-  measurementId: "G-P8Q82KR8W5"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
+console.log(firebaseConfig)
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
 export default app;
