67a62ae92b8cb0d437e4a1982ccc8e9f77c19b2e
michelleykim <yeojin011016@gmail.com>
2023-06-25 13:25:40 -0700

    add complete page and warning modal

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
-import React from "react";
+import React, { useState, useEffect, useLayoutEffect } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import { Provider } from "react-redux";
 import "./App.css";
 import Header from "./components/header";
 import Homepage from "./pages/homepage";
@@ -8,30 +7,58 @@ import AboutPage from "./pages/AboutPage";
 import WaitingPage from "./pages/WaitingPage";
 import GamePage from "./pages/GamePage";
 import LoginPage from "./pages/LoginPage";
+import { Modal } from "@mui/material";
+import CompletePage from "./pages/CompletePage";
+import Warning from "./components/Warning";
 
-import { createTheme, ThemeProvider } from "@mui/material";
-
-const theme = createTheme({
-    typography: {
-        fontFamily: ["Chilanka", "cursive"].join(","),
-    },
-});
+function useWindowSize() {
+  const [size, setSize] = useState(0);
+  useLayoutEffect(() => {
+    function updateSize() {
+      setSize(window.innerWidth);
+    }
+    window.addEventListener("resize", updateSize);
+    updateSize();
+    return () => window.removeEventListener("resize", updateSize);
+  }, []);
+  return size;
+}
 
 function App() {
-    return (
-        <>
-            <BrowserRouter>
-                <Header></Header>
-                <Switch>
-                    <Route exact path="/" component={Homepage} />
-                    <Route path="/about" component={AboutPage} />
-                    <Route path="/waiting/:sessionId" component={WaitingPage} />
-                    <Route path="/game" component={GamePage} />
-                    <Route path="/login" component={LoginPage} />
-                </Switch>
-            </BrowserRouter>
-        </>
-    );
+  const [open, isOpen] = useState(false);
+  const width = useWindowSize();
+
+  useEffect(() => {
+    if (width < 1024) {
+      isOpen(true);
+    } else {
+      isOpen(false);
+    }
+  }, [width]);
+
+  return (
+    <>
+      <BrowserRouter>
+        <Header></Header>
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route path="/about" component={AboutPage} />
+          <Route path="/waiting/:sessionId" component={WaitingPage} />
+          <Route path="/game" component={GamePage} />
+          <Route path="/login" component={LoginPage} />
+          <Route path="/complete/:sessionId" component={CompletePage} />
+        </Switch>
+      </BrowserRouter>
+      <Modal
+        aria-labelledby="modal-title"
+        aria-describedby="modal-description"
+        open={open}
+        onClose={() => isOpen(false)}
+      >
+        <Warning></Warning>
+      </Modal>
+    </>
+  );
 }
 
 export default App;
--- client/src/components/Timer.js
+++ client/src/components/Timer.js
@@ -1,58 +1,60 @@
 import React, { useEffect, useRef, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom/cjs/react-router-dom";
 
 import "./Timer.css";
 
 const Timer = ({ remainingTime }) => {
-    const currentTime = useRef(remainingTime);
-    const prevTime = useRef(null);
-    const isNewTimeFirstTick = useRef(false);
-    const [lastRerender, setOneLastRerender] = useState(0);
-    const history = useHistory();
+  const currentSession = useSelector((state) => state.currentSession);
+  const currentTime = useRef(remainingTime);
+  const prevTime = useRef(null);
+  const isNewTimeFirstTick = useRef(false);
+  const [lastRerender, setOneLastRerender] = useState(0);
+  const history = useHistory();
 
-    useEffect(() => {
-        if (remainingTime === 0) {
-            history.push(`/`);
-        }
-        return () => {
-            // Cleanup function to cancel any ongoing tasks or subscriptions
-            // Perform any necessary cleanup here
-        };
-    }, [lastRerender]);
-
-    if (currentTime.current !== remainingTime) {
-        isNewTimeFirstTick.current = true;
-        prevTime.current = currentTime.current;
-        currentTime.current = remainingTime;
-    } else {
-        isNewTimeFirstTick.current = false;
-    }
-
-    // force one last re-render when the time is over to tirgger the last animation
+  useEffect(() => {
     if (remainingTime === 0) {
-        setTimeout(() => {
-            setOneLastRerender((val) => val + 1);
-        }, 20);
-        return <div className="timer">Times Up!</div>;
+      history.push(`/complete/${currentSession.id}`);
     }
+    return () => {
+      // Cleanup function to cancel any ongoing tasks or subscriptions
+      // Perform any necessary cleanup here
+    };
+  }, [lastRerender]);
 
-    const isTimeUp = isNewTimeFirstTick.current;
+  if (currentTime.current !== remainingTime) {
+    isNewTimeFirstTick.current = true;
+    prevTime.current = currentTime.current;
+    currentTime.current = remainingTime;
+  } else {
+    isNewTimeFirstTick.current = false;
+  }
 
-    return (
-        <div className="time-wrapper">
-            <div key={remainingTime} className={`time ${isTimeUp ? "up" : ""}`}>
-                {remainingTime}
-            </div>
-            {prevTime.current !== null && (
-                <div
-                    key={prevTime.current}
-                    className={`time ${!isTimeUp ? "down" : ""}`}
-                >
-                    {prevTime.current}
-                </div>
-            )}
+  // force one last re-render when the time is over to tirgger the last animation
+  if (remainingTime === 0) {
+    setTimeout(() => {
+      setOneLastRerender((val) => val + 1);
+    }, 20);
+    return <div className="timer">Times Up!</div>;
+  }
+
+  const isTimeUp = isNewTimeFirstTick.current;
+
+  return (
+    <div className="time-wrapper">
+      <div key={remainingTime} className={`time ${isTimeUp ? "up" : ""}`}>
+        {remainingTime}
+      </div>
+      {prevTime.current !== null && (
+        <div
+          key={prevTime.current}
+          className={`time ${!isTimeUp ? "down" : ""}`}
+        >
+          {prevTime.current}
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default Timer;
