52ffeacf2a57c2e61c52c0b8ca9e53253f481a7e
martincai8 <martincai46@gmail.com>
2023-07-01 20:12:03 -0700

    Add image uploading

--- server/routes/sessions.js
+++ server/routes/sessions.js
@@ -1,5 +1,7 @@
 import express from "express";
 import { connectToDatabase } from "../db/conn.js";
+import ImageUploadService from "../services/ImageUploadService.js";
+import { ObjectId } from "mongodb";
 
 const router = express.Router();
 
@@ -18,10 +20,10 @@ router.get("/", async (req, res) => {
     const db = await connectToDatabase();
     const collection = db.collection("sessions");
     const sessions = await collection.find({}).toArray();
-    res.status(200).send(sessions);
+    return res.status(200).send(sessions);
   } catch (error) {
     console.error(error);
-    res.status(500).send({ error: "Internal Server Error" });
+    return res.status(500).send({ error: "Internal Server Error" });
   }
 });
 
@@ -51,10 +53,45 @@ router.post("/", async (req, res) => {
       finalImage: "",
     };
     const result = await collection.insertOne(newDocument);
-    res.status(200).send({ id: result.insertedId });
+    return res.status(200).send({ id: result.insertedId });
   } catch (error) {
     console.error(error);
-    res.status(500).send({ error: "Internal Server Error" });
+    return res.status(500).send({ error: "Internal Server Error" });
+  }
+});
+
+/**
+ * PATCH /:id/upload-drawing
+ * Uploads the session's drawing to Google Cloud Storage and updates the session's finalImage string with the public URL of the uploaded file.
+ *
+ * URL parameters:
+ * - id (string): The ID of the session to update.
+ *
+ * Request body parameters:
+ * - A multipart/form-data payload with a key of "img" and the value being the file to be uploaded.
+ *
+ * Returns:
+ * - A JSON object with a single property, publicUrl, containing the public URL of the uploaded file.
+ */
+router.patch("/:id/upload-drawing", async (req, res) => {
+  try {
+    const imageUploadService = new ImageUploadService();
+    const publicUrl = await imageUploadService.uploadFile(req, "drawings");
+
+    const db = await connectToDatabase();
+    const collection = db.collection("sessions");
+    const filter = { _id: new ObjectId(req.params.id) };
+    const updateDocument = { $set: { finalImage: publicUrl } };
+    const result = await collection.updateOne(filter, updateDocument);
+
+    if (result.modifiedCount === 0) {
+      return res.status(404).send({ error: "No session found with given id" });
+    }
+
+    return res.status(200).send({ publicUrl });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send(error);
   }
 });
 
---
+++ server/services/ImageUploadService.js
@@ -0,0 +1,64 @@
+import { Storage } from "@google-cloud/storage";
+import Multer from "multer";
+import { format } from "date-fns";
+import path from "path";
+
+class ImageUploadService {
+  constructor() {
+    this.multer = Multer({
+      storage: Multer.memoryStorage(),
+      limits: {
+        fileSize: 5 * 1024 * 1024 // limit files to 5MB
+      },
+    });
+
+    const storage = new Storage({
+      projectId: process.env.GCLOUD_PROJECT_ID,
+      credentials: {
+        client_email: process.env.GCLOUD_CLIENT_EMAIL,
+        private_key: process.env.GCLOUD_PRIVATE_KEY.replace(/\\n/g, "\n"),
+      },
+    });
+
+    this.bucket = storage.bucket("sketchconnect-images");
+  }
+
+  async uploadFile(req, folder) {
+    return new Promise((resolve, reject) => {
+      this.multer.single("img")(req, {}, async (error) => {
+        if (error) {
+          console.log("Multer error during upload: ", error);
+          reject(error);
+        } else {
+          try {
+            const timestamp = this.getFormattedTimestamp();
+            const extension = path.extname(req.file.originalname);
+            const fileName = `${timestamp}` + extension;
+            const blob = this.bucket.file(`${folder}/${fileName}`);
+            const blobStream = blob.createWriteStream();
+
+            blobStream.on("error", (error) => {
+              reject(error);
+            });
+
+            blobStream.on("finish", () => {
+              const publicUrl = `https://storage.googleapis.com/${this.bucket.name}/${blob.name}`;
+              resolve(publicUrl);
+            });
+
+            blobStream.end(req.file.buffer);
+          } catch (error) {
+            reject(error);
+          }
+        }
+      });
+    });
+  }
+
+  getFormattedTimestamp() {
+    const now = new Date();
+    return format(now, "MM-dd-yyyy-HH:mm:ss:SSS");
+  }
+}
+
+export default ImageUploadService;
