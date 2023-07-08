7fb4a18668cc701c3d362715b4532210ba22b98e
martincai8 <martincai46@gmail.com>
2023-07-08 01:13:46 -0700

    Fixes

--- server/server.js
+++ server/server.js
@@ -12,6 +12,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.status(200).send("Hello World");
+});
+
 app.use("/sessions", sessions);
 app.use("/users", users);
 
