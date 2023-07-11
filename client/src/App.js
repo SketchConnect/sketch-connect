7cc8271b8ee04a4b156e7fd2719c65332c809704
michelleykim <yeojin011016@gmail.com>
2023-07-11 14:44:39 -0700

    font change

--- client/src/App.js
+++ client/src/App.js
@@ -52,6 +52,9 @@ function App() {
           <Route path="/waitforturn/:sessionId" component={WaitingTurnPage} />
         </Switch>
       </BrowserRouter>
+      <link rel="preconnect" href="https://fonts.googleapis.com"/>
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+<link href="https://fonts.googleapis.com/css2?family=Chelsea+Market&display=swap" rel="stylesheet"/>
       <Modal
         aria-labelledby="modal-title"
         aria-describedby="modal-description"
