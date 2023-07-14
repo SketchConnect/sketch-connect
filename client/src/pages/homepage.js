6ef03e04b62f6596744c2fdd6985134d1052eae4
martincai8 <martincai46@gmail.com>
2023-07-13 18:10:14 -0700

    Add loading screen when server is waking up

--- client/src/pages/homepage.js
+++ client/src/pages/homepage.js
@@ -8,21 +8,25 @@ import { useNavigate } from "react-router-dom";
 import { setSession } from "../redux/session/reducer";
 
 function Homepage() {
-  const currentSessionId = useSelector((state) => state.session._id)
-  const tempUser = "648265d192b9bd82bbc849ed"
+  const currentSessionId = useSelector((state) => state.session._id);
+  const tempUser = "648265d192b9bd82bbc849ed";
   let [sessions, setSessions] = useState([]);
+  const [loading, setLoading] = useState(true);
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
   useEffect(() => {
     fetch("https://sketch-connect-be.onrender.com")
-      .then(() => console.log("Server is awake"))
+      .then(() => {
+        console.log("Server is awake");
+        setLoading(false);
+      })
       .catch((err) => console.log(`Failed to wake server: ${err}`));
   }, []);
 
   useEffect(() => {
     fetch("https://sketch-connect-be.onrender.com/sessions", {
-      method: "GET",
+      method: "GET"
     })
       .then((response) => {
         if (!response.ok) {
@@ -32,7 +36,8 @@ function Homepage() {
       })
       .then((fetchedSessions) => {
         setSessions(fetchedSessions);
-      }).catch(err => console.log(`Failed to fetch sessions: ${err}`));
+      })
+      .catch((err) => console.log(`Failed to fetch sessions: ${err}`));
   }, []);
 
   useEffect(() => {
@@ -49,7 +54,7 @@ function Homepage() {
       players: [tempUser]
     };
     dispatch(addSessionAsync(newSession)).then((session) => {
-      let payload = {session: session.payload, userId: tempUser}
+      let payload = { session: session.payload, userId: tempUser };
       dispatch(setSession(payload));
     });
   };
@@ -57,9 +62,28 @@ function Homepage() {
   const joinSession = (session) => {
     if (session.status === "waiting" && !session.players.includes(tempUser)) {
       dispatch(addPlayerAsync(session, tempUser));
-      let payload = {session: session, userId: tempUser}
-      dispatch(setSession(payload));      
-  }};
+      let payload = { session: session, userId: tempUser };
+      dispatch(setSession(payload));
+    }
+  };
+
+  if (loading) {
+    return (
+      <div className="loading-overlay">
+        <img
+          src={"assets/images/logo.png"}
+          alt="loading"
+          className="loading-image"
+        />
+        <div className="loading-text">
+          <span>Loading</span>
+          <span className="loading-dots">.</span>
+          <span className="loading-dots">.</span>
+          <span className="loading-dots">.</span>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="page">
@@ -99,6 +123,6 @@ function Homepage() {
       </div>
     </div>
   );
-};
+}
 
 export default Homepage;
