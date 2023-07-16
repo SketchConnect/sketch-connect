90dfc3034204c1cea9e703a72908c991c02c0f60
martincai8 <martincai46@gmail.com>
2023-07-16 14:54:04 -0700

    Fix typo

--- client/src/redux/session/service.js
+++ client/src/redux/session/service.js
@@ -14,7 +14,7 @@ const getSessions = async () => {
 const addSession = async (session) => {
   try {
     const response = await fetch(
-      "https://sketch-connect-be.onrender.com/sessions/sessions",
+      "https://sketch-connect-be.onrender.com/sessions",
       {
         method: "POST",
         headers: {
