08dbb20239dfc04d1429f72481202acb17535c6a
michelleykim <yeojin011016@gmail.com>
2023-07-18 14:08:52 -0700

    fix font issue for loading screen

--- client/src/App.js
+++ client/src/App.js
@@ -83,12 +83,6 @@ function App() {
           </Routes>
         </AuthContextProvider>
       </BrowserRouter>
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Chelsea+Market&display=swap"
-        rel="stylesheet"
-      />
       <Modal
         aria-labelledby="modal-title"
         aria-describedby="modal-description"
