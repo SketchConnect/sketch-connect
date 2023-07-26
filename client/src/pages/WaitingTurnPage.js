80b0db844d2d095e56909de75e19d600bbcccc92
martincai8 <martincai46@gmail.com>
2023-07-26 14:55:15 -0700

    Fix quadrants.length

--- client/src/pages/WaitingTurnPage.js
+++ client/src/pages/WaitingTurnPage.js
@@ -5,35 +5,40 @@ import { useSelector } from "react-redux";
 
 function WaitingTurnPage() {
   const currentSession = useSelector((state) => state.session);
-  const user = useSelector((state) => state.user._id)
+  const user = useSelector((state) => state.user._id);
   const navigate = useNavigate();
 
   useEffect(() => {
     let delay = 0;
 
-    fetch(`https://sketch-connect-be.onrender.com/sessions/${currentSession._id}`, {
-      method: "GET"
-    }).then((response) => {
-      if (!response.ok) {
-        throw new Error("HTTP error " + response.status);
+    fetch(
+      `https://sketch-connect-be.onrender.com/sessions/${currentSession._id}`,
+      {
+        method: "GET"
       }
-      return response.json();
-    }).then((response) => {
-      let currentTurn = response.quadrants.length();
-      let userIndex = response.players.indexOf(user)
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("HTTP error " + response.status);
+        }
+        return response.json();
+      })
+      .then((response) => {
+        let currentTurn = response.body.quadrants.length();
+        let userIndex = response.players.indexOf(user);
 
-      if (currentTurn > userIndex) {
-        delay = 10100 * (4 - currentTurn)
-        setTimeout(() => {
+        if (currentTurn > userIndex) {
+          delay = 10100 * (4 - currentTurn);
+          setTimeout(() => {
             navigate(`/complete/${currentSession._id}`);
-        }, delay);
-      } else if (currentTurn < userIndex) {
-        delay = 10100 * (userIndex - currentTurn);
-        setTimeout(() => {
+          }, delay);
+        } else if (currentTurn < userIndex) {
+          delay = 10100 * (userIndex - currentTurn);
+          setTimeout(() => {
             navigate(`/game/turn/${currentSession._id}`);
-        }, delay);
-      }
-    })
+          }, delay);
+        }
+      });
   }, []);
 
   return (
