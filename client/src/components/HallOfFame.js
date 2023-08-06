a4aaf67c99fadae475fc62edb95d12d3afb94311
Michelle W <michelle@dhcp-128-189-238-242.ubcsecure.wireless.ubc.ca>
2023-08-05 17:16:02 -0700

    remove reference to hall of fame css

--- client/src/components/HallOfFame.js
+++ client/src/components/HallOfFame.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import ImageCard from "./ImageCard";
-import "./HallOfFame.css";
 
 function HallOfFame() {
   const [showPopup, setShowPopup] = useState(false);
