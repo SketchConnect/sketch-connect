b3e710e64e5188b55bfcc0c9d95e4edd428965a0
Michelle W <michelle@Michelles-MacBook-Pro.local>
2023-08-08 21:18:51 -0700

    final cleanup

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -1,20 +1,16 @@
 import { useContext, createContext, useEffect } from "react";
 import {
   GoogleAuthProvider,
-  FacebookAuthProvider,
-  OAuthProvider,
   signInWithPopup,
   signOut,
   onIdTokenChanged
 } from "firebase/auth";
 import { auth } from "../firebase";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { addUserAsync } from "../redux/user/thunks";
 import { resetUser, setUser } from "../redux/user/reducer";
 
 const GoogleProvider = new GoogleAuthProvider();
-const FacebookProvider = new FacebookAuthProvider();
-const AppleProvider = new OAuthProvider("apple.com");
 
 const AuthContext = createContext();
 
@@ -23,27 +19,6 @@ export const AuthContextProvider = ({ children }) => {
 
   const googleSignIn = () => {
     signInWithPopup(auth, GoogleProvider)
-      .then((res) => {
-        console.log(res);
-      })
-      .catch((err) => {
-        console.log(err.message);
-      });
-  };
-
-  const facebookSignIn = () => {
-    console.log("into facebookSignIn in AuthContext");
-    signInWithPopup(auth, FacebookProvider)
-      .then((res) => {
-        console.log(res);
-      })
-      .catch((err) => {
-        console.log(err.message);
-      });
-  };
-
-  const appleSignIn = () => {
-    signInWithPopup(auth, AppleProvider)
       .then((res) => {
         console.log(res);
       })
@@ -66,10 +41,8 @@ export const AuthContextProvider = ({ children }) => {
           name: displayName,
           profilePic: photoURL
         };
-        console.log("user is", userToAdd);
         dispatch(addUserAsync(userToAdd)).then((user) => {
           let payload = { user: user };
-          console.log("the payload is ", payload);
           dispatch(setUser(payload));
         });
       } else {
@@ -83,9 +56,7 @@ export const AuthContextProvider = ({ children }) => {
   }, []);
 
   return (
-    <AuthContext.Provider
-      value={{ googleSignIn, facebookSignIn, appleSignIn, logOut }}
-    >
+    <AuthContext.Provider value={{ googleSignIn, logOut }}>
       {children}
     </AuthContext.Provider>
   );
